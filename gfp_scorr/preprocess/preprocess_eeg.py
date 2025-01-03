4f7f535d40b1244eaee30951c7d8d33f5506c0c2
smsadjadi <s.mostafa.sadjadi@gmail.com>
2025-01-03 20:13:08 +0330

    gfp_scorr modified for calculations on real EEG data

--- gfp_scorr/preprocess/preprocess_eeg.py
+++ gfp_scorr/preprocess/preprocess_eeg.py
@@ -1,6 +1,7 @@
 from scipy.signal import butter, filtfilt
+import numpy as np
 
-def preprocess_eeg(eeg_data, sfreq, low=1, high=30):
+def preprocess_eeg(eeg_data, sfreq, low=1, high=30, normalize=True):
     """
     Band-pass filter the EEG data.
 
@@ -18,6 +19,11 @@ def preprocess_eeg(eeg_data, sfreq, low=1, high=30):
         eeg_filtered: ndarray
             Band-pass filtered EEG data.
     """
+    
     b, a = butter(2, [low / (sfreq / 2), high / (sfreq / 2)], btype='band')
     eeg_filtered = filtfilt(b, a, eeg_data, axis=1)
+    
+    if normalize:
+        eeg_filtered = eeg_filtered / np.max(np.abs(eeg_filtered), axis=1, keepdims=True)
+        
     return eeg_filtered
